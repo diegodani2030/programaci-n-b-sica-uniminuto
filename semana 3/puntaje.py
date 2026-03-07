59a7f22ad7c20601049dbe5b4f15d28756ee79c9
diegodani2030 <140980076+diegodani2030@users.noreply.github.com>
2026-03-06 21:12:02 -0500

    Create puntaje.py

---
+++ semana 3/puntaje.py
@@ -0,0 +1,10 @@
+#comparar puntaje 1 con puntaje 2
+#pseudocodigo
+#inicio
+#leer puntaje1
+#leer puntaje2
+#escribir si puntaje 1 es mayor a puntaje2
+a = int(input("ingrese el puntaje de millonarios"))
+b = int(input("ingrese el puntaje nacional"))
+print(f'el puntaje de millanarios es mas alto que el de nacional{a<b}')
+print(f'el puntaje de millanarios es mas bajo que el de nacional{a>b}')
